6ca7fcaf520edcfc95145da2c11302a30d9110f9
Vincent <vincent.durret@gmail.com>
2023-07-03 19:42:40 +0200

    !!

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import Expenses from "./components/Expenses";
+import React from "react";
+
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -23,9 +26,15 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log("in app.js");
+    console.log(expense);
+  };
+
   return (
     <div>
       <h2>Fuck the World !!!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
